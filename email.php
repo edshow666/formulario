346b6d66f562336c018bbba60b2db11aa17c26a6
Edson Alexandre <49664253+edshow666@users.noreply.github.com>
2019-05-13 22:19:34 -0300

    Initial commit

---
+++ email.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['message']);
+
+
+$to = "edson_alexandre@outlook.com.br";
+$subject = "Contato - Formulário";
+$body = "Nome: ".$nome."\r\n".
+	"Email: ".$email."\r\n".
+	"Mensagem: ".$mensagem;
+$header	= "From:1012951@gmail.com"."\r\n".
+			"Reply-To:".$email."\r\n".
+			"X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)){
+
+	echo("Email enviado com sucesso!");
+
+} else{
+	echo("Email não pode ser enviado");
+}
+
+}
+
+?>
